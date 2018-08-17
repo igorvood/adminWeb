19c3c891097965d5d66c135786437d793c716623
balitsky_igor <balitsky_igor@list.ru>
2018-08-17 20:22:48 +0300

    Initial commit

---
+++ src/main/java/hello/Customer.java
@@ -0,0 +1,44 @@
+package hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    //@ViewColumn(displayName = "id_1")
+    private Long id;
+
+    @ViewColumn(displayName = "firstName_1")
+    private String firstName;
+
+    @ViewColumn(displayName = "lastName_колонка")
+    private String lastName;
+
+    @ViewColumn(displayName = "salary_колонка")
+    private long salary;
+
+    @ViewColumn(displayName = "salary_колонка")
+    private Date deteBirth;
+
+    @ViewColumn(displayName = "Тип колонка")
+    @ManyToOne
+    private TypeCustomer typeCustomer;
+
+}
---
+++ src/main/java/hello/TypeCustomer.java
@@ -0,0 +1,26 @@
+package hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeCustomer {
+    @Id
+    @GeneratedValue
+    //@ViewColumn(displayName = "id_1")
+    private Long id;
+
+    @ViewColumn(displayName = "Код")
+    private String code;
+
+}
